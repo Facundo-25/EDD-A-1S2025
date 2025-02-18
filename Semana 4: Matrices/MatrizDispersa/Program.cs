f2b2230ffd4c2f1c82d42f6aae68a253ea6dc2e6
Facundo-25 <3599878850101@ingenieria.usac.edu.gt>
2025-02-18 17:54:17 -0600

    Semana 4: Matrices

---
+++ Semana 4: Matrices/MatrizDispersa/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static unsafe void Main()
+        {
+
+            //Lista Doblemente Enlazada de VEHICULOS
+            LISTA_VEHICULO.insertar(VEHICULO)
+
+            //Lista Circular de REPUESTOS (CARGA MASIVA O DE LA INDIVIDUAL)
+            LISTA_REPUESTO.insertar(REPUESTO)
+
+
+
+
+            //al generar
+            //SERVICIO
+                //INPUT : ID-VEHICULO   3
+                //INPUT : ID-REPUESTO   7
+                //DETALLE: STRINGX
+
+
+                //pila de servicio 
+                //cola de facturacion
+                //matriz.insert(ID-VEHICULO,ID-REPUESTO,DETALLE)
+
+            
+            MatrizDispersa<int> matriz = new MatrizDispersa<int>(0);
+            //(pos_x, pos_y, nombre)
+            matriz.insert(3, 7, "1");
+            
+
+            matriz.mostrar();
+
+            
+            
+        }
+    }
+}
