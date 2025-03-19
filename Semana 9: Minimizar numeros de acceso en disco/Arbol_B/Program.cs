17b0e23b264e1ed349ebe40485d54c7f332fdd45
Steven Mejia <3599878850101@ingenieria.usac.edu.gt>
2025-03-19 15:11:24 -0600

    Semana 8: Ventajas de un arbol balanceado

---
+++ Semana 9: Minimizar numeros de acceso en disco/Arbol_B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BTree tree = new BTree();
+        int i = 1;
+
+        Console.WriteLine("Presiona Enter para insertar un nodo en el árbol. Escribe 'exit' para salir.");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "exit") 
+                break;
+
+            tree.Insert(new Factura(i, i, i * 100));
+            i++;
+
+            Console.WriteLine("Inserción de factura exitosa con ID: " + (i-1));
+            tree.GenerateGraphviz(); 
+        }
+
+        Console.WriteLine("Finalizando programa...");
+    }
+}
